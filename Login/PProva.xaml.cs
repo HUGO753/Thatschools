72aa54e6332f5860b6b4eb2ab3200b1814182f80
0040481621005 <0040481621005@LAB03IFTC15>
2018-09-17 10:01:10 -0300

    Adicionado A tela do professor e do aluno das provas, porem ainda possui um monte de bugs. Desculpa se os commits são muitos genericas, na proxima irei fazer melhor :)

---
+++ Login/PProva.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Login
+{
+    /// <summary>
+    /// Interaction logic for PProva.xaml
+    /// </summary>
+    public partial class PProva : Window
+    {
+        int codigo;
+        public PProva(int cod)
+        {
+            codigo = cod;
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void button_Copy1_Click(object sender, RoutedEventArgs e)
+        {
+            if ((Convert.ToInt32(tp_hr.Text) * 60 + Convert.ToInt32(tp_mn.Text)) != 0 && Titulo.Text != "" && Texto.Text != "")
+            {
+                DataSet tb = new DataSet();
+                OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Hugo\Desktop\Thatschool\bd.accdb"); // Conecta ao banco de dados
+                OleDbCommand cmd = new OleDbCommand("INSERT INTO prova(codigo_prof,titulo,texto,tempo) VALUES (" + codigo + ",'" + Titulo.Text + "','" + Texto.Text + "'," + (Convert.ToInt32(tp_hr.Text) * 60 + Convert.ToInt32(tp_mn.Text)) + ")");
+                cmd.Connection = con;
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Titulo.Text = "";
+                Texto.Text = "";
+                tp_hr.Text = "00";
+                tp_mn.Text = "00";
+            }
+            else
+            {
+                if (Titulo.Text == "")
+                {
+                    MessageBox.Show("Insira o titulo da prova!");
+                }
+                else if (Texto.Text == "")
+                {
+                    MessageBox.Show("Insira o texto!");
+                }
+                else
+                {
+                    MessageBox.Show("Insira o tempo de prova!");
+                }
+            }
+        }
+    }
+}
